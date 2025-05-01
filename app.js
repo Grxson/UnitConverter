fd2d078b99b211ce0eeaccd0d558a4d7c101803d
Grxson <gael.grxson@gmail.com>
2025-05-01 01:43:41 -0600

    arreglo de errores y vistas

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 import express from "express"
 import path from 'path'
+import expressLayouts from 'express-ejs-layouts';
 import { fileURLToPath } from "url"
 
+import lengthRoutes from './routes/length.routes.js';
+import weightRoutes from './routes/weight.routes.js';
+import temperatureRoutes from './routes/temperature.routes.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/length', lengthRoutes);
+app.use('/weight', weightRoutes);
+app.use('/temperature', temperatureRoutes);
 
 // Redirección base
 app.get('/', (req, res) => {
     res.redirect('/length');
 });
 
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
